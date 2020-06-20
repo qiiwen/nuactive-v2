45c68f75b99dfbf8f1b9527de64212d3fb231c79
qiiwen <qiiwen@users.noreply.github.com>
2020-06-20 23:34:26 +0800

    test2

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,43 +1,4 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-// import axios from axios
-
-// export const addToList = term => {
-//   return axios
-//       .post(
-//           'api/task', {
-//               title: term
-//           }, {
-//               headers: { "Content-type": "application/json" }
-//           })
-//       .then((res) => {
-//           console.log(res)
-//       })
-// }
-
-// class List extends Component {
-//   constructor() {
-//       super()
-//       this.state = {
-//           id: '',
-//           term: '',
-//           editDisabled: false,
-//           items: []
-//       }
-
-//       this.onSubmit = this.onSubmit.bind(this)
-//   }
-
-//   onSubmit = e => {
-//       e.preventDefault()
-//       this.setState({ editDisabled: '' })
-//       addToList(this.state.term).then(() => {
-//           this.getAll()
-//       })
-//   }
-
-// }
-=======
 import { Box } from "grommet";
 >>>>>>> c046ce492b8b92d9d821ecb7ce97ab1b6553c0ba
 
