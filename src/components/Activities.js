07bbfadc69725cb90183fa31d0ff0d7d6c3de433
elicialzy <65524472+elicialzy@users.noreply.github.com>
2020-06-20 22:18:28 +0800

    Update Activities.js

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
+import { Box } from "grommet";
 
 class Activities extends Component {
   render() {
     return (
       <div className="container">
-        <div className="jumbotron mt-5">
+        <div className="jumbotron mt-5" style={jumbotronStyle}>
           <div className="col-sm-8 mx-auto">
-            <h1 className="text-center">ACTIVITIES</h1>
+            <h1 className="text-center" style={h1Style}>
+              ACTIVITIES
+            </h1>
           </div>
         </div>
+        <Box pad="large" background="#F2EEF9" style={boxStyle}>
+          <p class="para" style={paraStyle}>
+            classesssss
+          </p>
+        </Box>
       </div>
     );
   }
 }
+
+const jumbotronStyle = {
+  background: "#410543",
+};
+
+const h1Style = {
+  color: "white",
+};
+
+const boxStyle = {
+  padding: "10px",
+};
+
+const paraStyle = {
+  padding: "10px",
+  margin: "10px 20px 10px 20px",
+  fontSize: "17px",
+  text: "align",
+  textAlign: "justify",
+};
 
 export default Activities;
